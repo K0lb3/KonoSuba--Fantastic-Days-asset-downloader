881af16b84cdd0636076339d6c3198a4694537e9
K0lb3 <rkolbe96@gmail.com>
2021-11-19 13:42:45 +0100

    fix server request error because of too much data was requested at once

--- update_master.py
+++ update_master.py
@@ -11,15 +11,18 @@ def main():
 
     print("fetching masterdata")
     master_version = masterlist["masterversion"]
-    masterall = game_api.masterall(
-        [item["master_key"] for item in masterlist["masterarray"]]
-    )
+
+    masterdata = []
+    master_keys = [item["master_key"] for item in masterlist["masterarray"]]
+    for i in range(0, len(master_keys), 10):
+        keys = master_keys[i : i + 10]
+        masterdata.extend(game_api.masterall(keys)["masterarray"])
 
     print("dumping masterdata")
     ROOT = os.path.dirname(os.path.realpath(__file__))
     folder = os.path.join(ROOT, "master")
     os.makedirs(folder, exist_ok=True)
-    for item in masterall["masterarray"]:
+    for item in masterdata:
         print(item["master_key"])
         with open(
             os.path.join(folder, f"{item['master_key']}.json"), "wt", encoding="utf8",
